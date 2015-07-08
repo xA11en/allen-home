aeb57725ca2e4e671a1857d13f6ba79311a46e25
chenjl <chenjl@075e640e-6597-4097-b81a-f66ed52b749f>
2015-07-08 03:57:31 +0000

    git-svn-id: https://192.168.1.42/nbs-automation-test/trunk@77 075e640e-6597-4097-b81a-f66ed52b749f

--- auto-web/ty-qa-autoweb/src/main/java/com/tingyun/auto/reporter/HTMLReporter.java
+++ auto-web/ty-qa-autoweb/src/main/java/com/tingyun/auto/reporter/HTMLReporter.java
@@ -309,7 +309,7 @@ public class HTMLReporter extends AbstractReporter{
 		+ "auto"
 		+ "环境      "
 		+ bundle.getString("subject");
-		HtmlMail.send(toMail, subject, html.toString());
+		//HtmlMail.send(toMail, subject, html.toString());
 
 	}
 
