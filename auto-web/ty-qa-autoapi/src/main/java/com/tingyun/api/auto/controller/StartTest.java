fc808a9fdeb4b00476e6ee8172f63888828fd699
chenjl <chenjl@075e640e-6597-4097-b81a-f66ed52b749f>
2015-08-21 03:48:56 +0000

    git-svn-id: https://192.168.1.42/nbs-automation-test/trunk@204 075e640e-6597-4097-b81a-f66ed52b749f

--- auto-web/ty-qa-autoapi/src/main/java/com/tingyun/api/auto/controller/StartTest.java
+++ auto-web/ty-qa-autoapi/src/main/java/com/tingyun/api/auto/controller/StartTest.java
@@ -2,6 +2,7 @@ package com.tingyun.api.auto.controller;
 
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.tmatesoft.sqljet.core.internal.lang.SqlParser.match_op_return;
 
 import com.tingyun.api.auto.dao.impl.MarkingImpl;
+import com.tingyun.api.auto.utils.HtmlMail;
 import com.tingyun.api.auto.utils.SVNUtils;
 
 
@@ -26,37 +28,30 @@ import com.tingyun.api.auto.utils.SVNUtils;
 @Controller
 public class StartTest {
 	private static boolean flag = false;
+	//本地副本代码路径
+	private static File filedir = null;
+	private static long workVersion = -1;
 	private static Logger LOG = LoggerFactory.getLogger(StartTest.class);
 	@RequestMapping("/go")
 	public String startTest(ModelMap modelMap,HttpServletRequest request) {
-				try {
-					SVNUtils.checkCodeFromSvn();
-				Thread.sleep(1000);
-				Runtime process = Runtime.getRuntime();
-				process.exec("cmd /c start");
-				Thread.sleep(2000);
-				Robot r = new Robot();
-					r.keyPress(KeyEvent.VK_C);
-					r.keyPress(KeyEvent.VK_D);
-					r.keyPress(KeyEvent.VK_SPACE);
-					r.keyPress(KeyEvent.VK_L);
-					r.keyPress(KeyEvent.VK_ENTER);
-					r.keyPress(KeyEvent.VK_M);
-					r.keyPress(KeyEvent.VK_V);
-					r.keyPress(KeyEvent.VK_N);
-					r.keyPress(KeyEvent.VK_SPACE);
-					r.keyPress(KeyEvent.VK_T);
-					r.keyPress(KeyEvent.VK_E);
-					r.keyPress(KeyEvent.VK_S);
-					r.keyPress(KeyEvent.VK_T);
-					r.keyPress(KeyEvent.VK_ENTER);
-					//退出
-					r.keyPress(KeyEvent.VK_E);
-					r.keyPress(KeyEvent.VK_X);
-					r.keyPress(KeyEvent.VK_I);
-					r.keyPress(KeyEvent.VK_T);
-					r.keyPress(KeyEvent.VK_ENTER);
-					modelMap.addAttribute("msg", "测试正在后台执行，点击查看报告后可能需要耐心等待一点时间！....");
+			try {
+				//创建本地代码副本路径
+				filedir = new File(HtmlMail.LOCAL_SVN_WORKSPACE);
+				if (filedir.exists()) {
+					if(workVersion == SVNUtils.getLastVersionNum()){
+						goTest();
+						modelMap.addAttribute("msg", "测试正在后台执行，点击查看报告后可能需要耐心等待一点时间！....");
+					}else{
+						LOG.warn("您本地已存在相同路径：{}即将做更新操作....",filedir.getAbsolutePath());
+						SVNUtils.updateCodeFromSvn(filedir);
+						goTest();
+						modelMap.addAttribute("msg", "测试正在后台执行，点击查看报告后可能需要耐心等待一点时间！....");
+					}
+				}else{
+					filedir.mkdir();
+					workVersion =SVNUtils.checkCodeFromSvn(filedir);
+					goTest();
+				}
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					LOG.info("执行测试异常",e);
@@ -89,5 +84,33 @@ public class StartTest {
 			// TODO: handle exception
 			throw new RuntimeException(e);
 		}
+	}
+	
+	private void goTest()throws Exception{
+		Thread.sleep(1000);
+		Runtime process = Runtime.getRuntime();
+		process.exec("cmd /c start");
+		Thread.sleep(2000);
+		Robot r = new Robot();
+			r.keyPress(KeyEvent.VK_C);
+			r.keyPress(KeyEvent.VK_D);
+			r.keyPress(KeyEvent.VK_SPACE);
+			r.keyPress(KeyEvent.VK_L);
+			r.keyPress(KeyEvent.VK_ENTER);
+			r.keyPress(KeyEvent.VK_M);
+			r.keyPress(KeyEvent.VK_V);
+			r.keyPress(KeyEvent.VK_N);
+			r.keyPress(KeyEvent.VK_SPACE);
+			r.keyPress(KeyEvent.VK_T);
+			r.keyPress(KeyEvent.VK_E);
+			r.keyPress(KeyEvent.VK_S);
+			r.keyPress(KeyEvent.VK_T);
+			r.keyPress(KeyEvent.VK_ENTER);
+			//退出
+			r.keyPress(KeyEvent.VK_E);
+			r.keyPress(KeyEvent.VK_X);
+			r.keyPress(KeyEvent.VK_I);
+			r.keyPress(KeyEvent.VK_T);
+			r.keyPress(KeyEvent.VK_ENTER);
 	}
 }
